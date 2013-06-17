1bae1232069e5939ce7c85d1597fcab4d4f685d2
Imran Zaman <imran.zaman@intel.com>
2013-06-17 18:12:49 +0300

    changes made to distribution specific files

---
+++ tools/prepare-tizen.sh
@@ -0,0 +1,22 @@
+# $1 corresponds to libgsignon-glib-<ver>.tar.gz 
+# $2 is the destination folder
+# NOTE: all the files will be extracted under destination folder (instead of destfolder/libgsignon-glib-<version>)
+
+if [ $# -ne 2 -o -z "$1" -o -z "$2" ]; then
+    echo "Invalid arguments supplied"
+    echo "Usage: ./prepare-tizen.sh libgsignon-glib-<version>.tar.gz destfolder"
+    echo "NOTE: All the files will be extracted under destfolder (instead of destfolder/libgsignon-glib-<version>)"
+    exit
+fi
+
+currdir = `pwd`;
+echo "CURR dir = $currdir"
+
+mkdir -p $2 && \
+tar -xzvf $1 -C $2 --strip-components 1 && \
+cd $2 && \
+mkdir -p packaging && \
+cd packaging && \
+cp -f ../dists/rpm/libgsignon-glib-tizen.spec libgsignon-glib.spec &&
+cp -f ../dists/rpm/libgsignon-glib-tizen.changes libgsignon-glib.changes;
+
