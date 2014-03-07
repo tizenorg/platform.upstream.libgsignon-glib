264fe7ba323f287174f66d30433b80916864395e
Imran Zaman <imran.zaman@intel.com>
2014-03-07 14:06:05 +0200

    fixed tizen prepare script for packaging folder

--- tools/prepare-tizen.sh
+++ tools/prepare-tizen.sh
@@ -4,21 +4,20 @@
 
 if [ $# -ne 2 -o -z "$1" -o -z "$2" ]; then
     echo "Invalid arguments supplied"
-    echo "Usage: ./prepare-tizen.sh libgsignon-glib-<version>.tar.gz destfolder"
+    echo "Usage: ./prepare-tizen.sh libgsignon-glib-<version>.tar.gz /absolute/path/to/destfolder"
     echo "NOTE: All the files will be extracted under destfolder (instead of destfolder/libgsignon-glib-<version>)"
     exit
 fi
 
-currdir = `pwd`;
+currdir=`pwd`;
 echo "CURR dir = $currdir"
 
 mkdir -p $2 && \
 cd $2 && \
-git rm -r * && \
-tar -xzvf $1 -C $2 --strip-components 1 && \
+git rm -r *; rm -rf packaging;
+tar -xzvf $currdir/$1 -C $2 --strip-components 1 && \
 mkdir -p packaging && \
-cd packaging && \
-ln -s ../dists/rpm/libgsignon-glib-tizen.spec libgsignon-glib.spec && \
-ln -s ../dists/rpm/libgsignon-glib-tizen.changes libgsignon-glib.changes && \
-cd .. && git add *;
-
+cp -f dists/rpm/libgsignon-glib-tizen.spec packaging/libgsignon-glib.spec && \
+cp -f dists/rpm/libgsignon-glib-tizen.changes packaging/libgsignon-glib.changes && \
+cp $currdir/.gitignore $2/ && \
+git add -f *;
